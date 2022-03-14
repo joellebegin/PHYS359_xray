f8c00f624b1938441fe5fbe26a8c03f62d04824f
Joelle Begin <joellebegin.6@gmail.com>
2022-03-13 22:34:10 -0400

    changing fit model

--- readData.py
+++ readData.py
@@ -1,7 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
-plt.ion()
+from scipy import optimize, signal
+
+import math
+import random
+
+from lmfit import models
+
+# plt.ion()
 
 '''
 WISHLIST: add a nice plotting function
@@ -9,71 +15,152 @@ WISHLIST: add a nice plotting function
 
 
 dat = np.loadtxt("Cu_03-09-20.UXD")
-theta = dat[:,0]
-cts = dat[:,1]
 
-def lor(x,B,G,mu):
-    lor = np.abs(B)*G/(2*np.pi*( (x - mu)**2 + (0.5*G)**2) )
-    return lor
+def peak_finder(spec, model_indicies, distance, height, **kwargs):
+    '''
+    Finds peaks in given data. 
 
-def gaus(x,A,mu,sig):
-    gaus = np.abs(A)*(np.exp(-0.5*(x - mu)**2/sig**2))/(sig*np.sqrt(np.pi*2))
-    return gaus
+    Params:
+    -------
+    spec: dictionary
+        dictionary w/ data + fit models
 
-def gaus_and_lor(x,A,B,G, mu, sig, c):
-    l = lor(x,B,G,mu)
-    g = gaus(x,A,mu,sig)
+    model_indices: list
+        just a list indexing the models
 
-    return l + g + c
+    distance: int
+        minimum distance between peaks. Prob dont need here since
+        only doing one peak at a time
 
-def red_chi2(data, fit, errors, n_params):
-    return np.sum((data - fit)**2/errors**2)/(len(data) - n_params)
+    height: height of peaks
+    '''
+    x = spec['x']
+    y = spec['y']
+    x_range = np.max(x) - np.min(x)
+    peak_indicies,_ = signal.find_peaks(y, distance=distance, height=height)
+    np.random.shuffle(peak_indicies)
+    for peak_indicie, model_indicie in zip(peak_indicies.tolist(), model_indicies):
+        model = spec['model'][model_indicie]
+        if model['type'] in ['GaussianModel', 'LorentzianModel', 'VoigtModel']:
+            params = {
+                'height': y[peak_indicie],
+                'sigma': x_range / len(x),
+                'center': x[peak_indicie]
+            }
+            if 'params' in model:
+                model.update(params)
+            else:
+                model['params'] = params
+        else:
+            raise NotImplemented(f'model {basis_func["type"]} not implemented yet')
+    return peak_indicies
 
-if True:
-    fig = plt.figure()
-    plt.plot(theta, cts)
-    plt.xlabel("2*theta [deg]")
-    plt.ylabel("Intensity [counts]")
+def generate_model(spec):
+    '''
+    I have absolutely no idea what this function does, and don't feel like deciphering it
+    to add good comments. Idk this whole framework seems kinda overkill-- was nice
+    when we were trying to fit all peaks at once, but now we're doing one at a time.
+    I think we could probably write a way simpler version???
+    '''
+
+    composite_model = None
+    params = None
+    x = spec['x']
+    y = spec['y']
+    x_min = np.min(x)
+    x_max = np.max(x)
+    x_range = x_max - x_min
+    y_max = np.max(y)
+    for i, basis_func in enumerate(spec['model']):
+        prefix = f'm{i}_'
+        model = getattr(models, basis_func['type'])(prefix=prefix)
+        if basis_func['type'] in ['GaussianModel', 'LorentzianModel', 'VoigtModel']: # for now VoigtModel has gamma constrained to sigma
+            model.set_param_hint('sigma', min=1e-6, max=x_range)
+            model.set_param_hint('center', min=x_min, max=x_max)
+            model.set_param_hint('height', min=1e-6, max=1.1*y_max)
+            model.set_param_hint('amplitude', min=1e-6)
+            # default guess is horrible!! do not use guess()
+            default_params = {
+                prefix+'center': x_min + x_range * random.random(),
+                prefix+'height': y_max * random.random(),
+                prefix+'sigma': x_range * random.random()
+            }
+        else:
+            raise NotImplemented(f'model {basis_func["type"]} not implemented yet')
+        if 'help' in basis_func:  # allow override of settings in parameter
+            for param, options in basis_func['help'].items():
+                model.set_param_hint(param, **options)
+        model_params = model.make_params(**default_params, **basis_func.get('params', {}))
+        if params is None:
+            params = model_params
+        else:
+            params.update(model_params)
+        if composite_model is None:
+            composite_model = model
+        else:
+            composite_model = composite_model + model
+    return composite_model, params
+
+
+def fit_one_peak(lims):
+    '''
+    Fits just one peak. 
+
+    Params:
+    -------
+    lims: 2d array/list
+        Gives window for peak
+
+    Returns:
+    --------
+    '''
+    #clipping data according to given limits
+    clip_dat = np.array([i for i in dat if (i[0]>lims[0] and i[0]<lims[1])]).T
+
+    spec = {
+        'x': clip_dat[0],
+        'y': clip_dat[1],
+        'model': [
+            {'type': 'VoigtModel'},
+            {'type': 'VoigtModel'}
+        ]
+    }
+
+    peaks_found = peak_finder(spec, [0, 1], distance=20, height=20,sharex=True)
+    errs = np.sqrt(clip_dat[1])
+    mask = errs != 0
+
+    max_iter = 300
+    thresh = 3.5
+    for i in range(max_iter):
+        model, params = generate_model(spec)
+        output = model.fit(spec['y'], params, x=spec['x'])
+        chi2 = np.sum((clip_dat[1][mask]-output.best_fit[mask])**2/errs[mask])/(len(clip_dat[0][mask])-len(params))
+        if chi2 <= thresh:
+            # print('chi2 =',np.round(chi2,2),'accepted')
+            break
+        elif chi2 > thresh:
+            # print('chi2 =',np.round(chi2,2),'rejected')
+            if i==max_iter-1:
+                print('Did not find a model satisfying chi2 <=',thresh)
+
+        
+    fig, ax = plt.subplots(nrows=2, ncols = 1, gridspec_kw={'height_ratios': [3,1]})
+    plt.subplots_adjust(hspace=0)
+
+    ax[0].plot(clip_dat[0],output.best_fit,color='r',label=np.round(chi2,2))
+    ax[0].plot(clip_dat[0],clip_dat[1],label='data')
+    ax[0].set_ylabel("Intensity [counts]")
+    ax[0].legend()
+
+    ax[1].errorbar(clip_dat[0], clip_dat[1]-output.best_fit, yerr = errs, ls = " ", marker = ".", capsize=2)
+    ax[1].set_xlabel(r"$2\theta$ [deg]")
+    ax[1].set_ylabel("Residuals")
+    ax[1].hlines(xmin = clip_dat[0][0], xmax = clip_dat[0][-1], y = 0, color = "k")
+
+    ax[0].scatter(clip_dat[0][peaks_found],clip_dat[1][peaks_found],marker='*')
     plt.show()
 
-def fit_and_plot(lim, p0, bounds, plot = True, plot_savepath=None):
-
-    clip_dat = np.array([i for i in dat if (i[0]>lim[0] and i[0]<lim[1])]).T
-    pars, cov = curve_fit(gaus_and_lor, clip_dat[0], clip_dat[1], p0=p0, bounds=bounds)
-    
-    print(pars)
-    
-    errs = np.sqrt(clip_dat[1])
-    fit = gaus_and_lor(clip_dat[0], *pars)
-    res = clip_dat[1] - fit
-
-    mask = errs != 0
-    chi = red_chi2(clip_dat[1][mask], fit[mask], errs[mask], len(pars))
-
-    if plot:
-        thet_linspace = np.linspace(clip_dat[0][0], clip_dat[0][-1], 10000)
-        fit_linsp = gaus_and_lor(thet_linspace, *pars)
-
-
-        fig, ax = plt.subplots(nrows=2, ncols = 1, gridspec_kw={'height_ratios': [3,1]})
-        plt.subplots_adjust(hspace=0)
-
-        ax[0].errorbar(clip_dat[0], clip_dat[1], yerr = errs, color = "k", linestyle = "", marker = ".", label = "Chi^2: %f"%(chi), capsize=2)
-        ax[0].plot(thet_linspace, fit_linsp)
-        # ax[0].plot(thet2, gaus(thet2, pars[0], pars[3],pars[4]) + pars[5], label = "gaussian")
-        # ax[0].plot(thet2, lor(thet2, pars[1], pars[2], pars[3]) + pars[5], label ="lor", color = "red")
-        ax[0].set_ylabel("Intensity [counts]")
-        ax[0].legend()
-
-        ax[1].errorbar(clip_dat[0], res, yerr = errs, ls = " ", marker = ".", capsize=2)
-        ax[1].set_xlabel("2*theta [deg]")
-        ax[1].set_ylabel("Residuals")
-        ax[1].hlines(xmin = lim[0], xmax = lim[1], y = 0, color = "k")
-        
-        if plot_savepath is not None:
-            plt.savefig(plot_savepath, bbox_inches = "tight")
-        else:
-            plt.show()
 
 
 #peak centers
@@ -83,29 +170,13 @@ p3 = 74.2
 p4 = 90
 p5 = 95
 
-pars = [
-    [800,800,0.5,p1,0.5, 5],
-    [500,500,0.5,p2,0.5, 5],
-    [200,200,0.5,p3,0.5, 5],
-    [100,100,0.5,p4,0.5, 5],
-    [50,50,0.5,p5,0.5, 5]
-]
-
-bounds = [
-([100,0,0,0,0,0],[np.inf, np.inf, np.inf, np.inf, 1, np.inf]),
-([100,0,0,0,0,0],[np.inf, np.inf, np.inf, np.inf, 1, np.inf]),
-([100,0,0,0,0,0],[np.inf, np.inf, np.inf, np.inf, 1, np.inf]),
-([1,0,0,0,0,0],[np.inf, np.inf, np.inf, np.inf, 1, np.inf]),
-([1,0,0,0,0,0],[np.inf, np.inf, np.inf, np.inf, 1, np.inf])
-]
-
+win_len = 2
 lims = [
-    [p1-3,p1+3],
-    [p2-3,p2+3],
-    [p3-3,p3+3],
-    [p4-3,p4+3],
-    [p5-3,p5+3]
+    [p1-win_len,p1+win_len],
+    [p2-win_len,p2+win_len],
+    [p3-win_len,p3+win_len],
+    [p4-win_len,p4+win_len],
+    [p5-win_len,p5+win_len]
 ]
 
-for i in range(len(pars)):
-    fit_and_plot(lims[i], pars[i], bounds[i], plot_savepath=f"peak_{i}.png")
+print(fit_one_peak(lims[1]))
