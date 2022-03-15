59e594d62bf2b85990a77e79234f84bb374af9f5
AlexLaroche7 <alexander.laroche@mail.mcgill.ca>
2022-03-15 13:24:16 -0400

    correct errors

--- lattice_param_fitting.py
+++ lattice_param_fitting.py
@@ -17,7 +17,7 @@ for i,material in enumerate(materials):
         fits[i] = pickle.load(handle)
     
     angles[i] = np.array([fits[i]['popt'][j][1] for j in range(len(fits[i]['popt']))])/2
-    errs[i] = np.array([fits[i]['pcov'][j][1][1] for j in range(len(fits[i]['pcov']))])
+    errs[i] = np.array([fits[i]['pcov'][j][1][1] for j in range(len(fits[i]['pcov']))])/2
 
 fig, ax = plt.subplots(nrows=2, ncols = 1, gridspec_kw={'height_ratios': [3,1]}, figsize=(10,5))
 plt.subplots_adjust(hspace=0)
