ca638dbd62053a34882bb6f7f782702605ceec99
AlexLaroche7 <alexander.laroche@mail.mcgill.ca>
2022-03-18 12:55:42 -0400

    Pb Sn fitting

--- Pb_Sn.py
+++ Pb_Sn.py
@@ -3,12 +3,13 @@ import matplotlib.pyplot as plt
 
 from readData import voigt,fit_peak,fit_n_peaks
 
-def get_bds(mus,width):
+def get_bds(mus,widths):
     bds=[[0,0]]*len(mus)
     for i in range(len(bds)):
         bds[i] = [mus[i]-widths[i],mus[i]+widths[i]]
     return bds
 
+'''
 # FIT PB DATA (trivial since it's still FCC)
 
 Pb_dat = np.loadtxt('data/Pb_10-03-22.UXD')
@@ -24,9 +25,11 @@ fit_n_peaks(Pb_dat,bds,mus,amps,'Pb')
 
 Sn_dat = np.loadtxt('data/Sn_10-03-22.UXD')
 
-mus = [31.4,36.3,52.3,62.2,65.3,77.1,85.5,88.4,99.5,108]
-widths = [0.8]*len(mus)
+mus=[30.7,32.1,44,45,55.4,62.65,63.9,64.65,72.45,73.25]
+widths=[0.6,0.6,0.6,0.6,0.8,0.4,0.35,0.4,0.4,0.4]
 bds=get_bds(mus,widths)
-amps = [100,100,10,10,2,2,2,2,2,2]
+amps = [100,100,30,50,5,10,2,5,5,5]
 
-fit_n_peaks(Sn_dat,bds,mus,amps,'Sn')
+fit_n_peaks(Sn_dat,bds,mus,amps,'Sn')'''
+
+# FIT PB25 SN75 DATA (have to start doing multiple peak fits)
